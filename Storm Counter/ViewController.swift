e218407b7bcb22ab145b42b7d497a4524b835d9b
Godohaldo Perez <godohaldoperez@godohaldos-macbook-pro.local>
2018-06-05 22:48:02 -0500

    Initial Commit

---
+++ Storm Counter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Storm Counter
+//
+//  Created by Godohaldo Perez on 6/5/18.
+//  Copyright © 2018 Godohaldo Perez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
